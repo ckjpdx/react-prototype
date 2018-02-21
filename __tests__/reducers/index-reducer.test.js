f014b72ec360bc067c5bb94263e1760a3ad10a32
Chris Knight Johnson <cknightjohnson@gmail.com>
2018-02-20 19:42:10 -0800

    add moment to add ticket

--- __tests__/reducers/index-reducer.test.js
+++ __tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from './../../src/reducers/index';
 import selectedTicketReducer from './../../src/reducers/selected-ticket-reducer';
 import ticketListReducer from './../../src/reducers/ticket-list-reducer';
 import { createStore } from 'redux';
+import Moment from 'moment';
 
 let store = createStore(rootReducer);
 
@@ -17,6 +18,29 @@ describe("rootReducer", () => {
 
   test('Should contain selectedTicketReducer logic', () => {
     expect(store.getState().selectedTicket).toEqual(selectedTicketReducer(undefined, { type: null }));
+  });
+  
+  test('New ticket should include Moment-formatted wait times', () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id,
+      formattedWaitTime: new Moment().fromNow(true)
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: 'a few seconds'
+      }
+    });
   });
 
 });
--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,18 +1,30 @@
 export default (state = {}, action) => {
+  let newState;
+  const { names, location, issue, timeOpen, id, formattedWaitTime } = action;
+
   switch (action.type){
     case 'ADD_TICKET':
     const {names, location, issue, timeOpen, id} = action;
-    let newState = Object.assign({}, state, {
-      [id]: {
-        names: names,
-        location: location,
-        issue: issue,
-        timeOpen: timeOpen,
-        id: id
-      }
+      newState = Object.assign({}, state, {
+        [id]: {
+          names: names,
+          location: location,
+          issue: issue,
+          timeOpen: timeOpen,
+          id: id,
+          formattedWaitTime: formattedWaitTime          
+        }
     });
     console.log('ADD_TICKET in reducer');
     return newState;
+
+    case 'UPDATE_TIME':
+      const newTicket = Object.assign({}, state[id], {formattedWaitTime});
+      newState = Object.assign({}, state, {
+        [id]: newTicket
+      });
+      return newState;
+
   default:
     return state;
   }
